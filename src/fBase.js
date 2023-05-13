6c35ac492e6b3175e1dc15c955d8f4755c1df13f
chmh0805 <chmh0805@gmail.com>
2023-05-13 10:56:44 +0900

    feat: Form and Database Setup

--- src/fBase.js
+++ src/fBase.js
@@ -8,6 +8,8 @@ import {
 	GithubAuthProvider,
 } from "firebase/auth";
 
+import "firebase/database";
+
 const firebaseConfig = {
 	apiKey: process.env.REACT_APP_API_KEY,
 	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+	const [hweet, setHweet] = useState("");
+	const onSubmit = (event) => {
+		event.preventDefault();
+	};
+	const onChange = (event) => {
+		const {
+			target: { value },
+		} = event;
+		setHweet(value);
+	};
+	return (
+		<div>
+			<form onSubmit={onSubmit}>
+				<input
+					value={hweet}
+					onChange={onChange}
+					type="text"
+					placeholder="What's on your mind?"
+					maxLength={120}
+				/>
+				<input type="submit" value="Hweet" />
+			</form>
+		</div>
+	);
+};
 export default Home;
